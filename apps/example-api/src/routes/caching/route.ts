40d446e0d7219c6a1dde348b6e73fece02f11e3b
Steve Higgs <steve@speechify.com>
2025-11-15 16:53:40 +0000

    cache a more complex object

--- apps/example-api/src/routes/caching/route.ts
+++ apps/example-api/src/routes/caching/route.ts
@@ -1,27 +1,48 @@
 import { randomUUID } from 'node:crypto';
 import type { EnhancedFastifyInstance } from '@repo/fastify-base';
 import { createInMemoryCache } from '@repo/fastify-base/caching';
+import z from 'zod';
 
 const cache = createInMemoryCache({
-  ttl: '5s',
+  ttl: '20s',
   maxSize: 10
 });
 
-export default function registerRoutes(app: EnhancedFastifyInstance) {
-  app.get('/caching', async (req, reply) => {
-    // try to get the uuid from the cache
-    const cachedUuid = await cache.getItem<string>('test-uuid');
-    if (cachedUuid) {
-      req.log.info({ message: 'cached hit', uuid: cachedUuid });
+const cachedItemSchema = z.object({
+  uuid: z.string(),
+  timestamp: z.date()
+});
 
-      return reply.send({ uuid: cachedUuid });
+const requestSchema = z.object({
+  response: {
+    200: cachedItemSchema
+  }
+});
+
+type CachedItem = z.infer<typeof cachedItemSchema>;
+
+export default function registerRoutes(app: EnhancedFastifyInstance) {
+  app.get('/caching', { schema: requestSchema }, async (req, reply) => {
+    // try to get the uuid from the cache
+    const cachedItem = await cache.getItem<CachedItem>('test-item');
+    if (cachedItem) {
+      req.log.info({ message: 'cached hit', item: cachedItem });
+
+      return reply.send(cachedItem);
     }
 
     // not found, so generate a new uuid
     const uuid = randomUUID();
+    const timestamp = new Date();
     req.log.info({ message: 'cache miss', uuid });
-    cache.setItem('test-uuid', uuid);
+    cache.setItem<CachedItem>('test-item', {
+      uuid,
+      timestamp: new Date()
+    });
 
-    reply.send({ uuid });
+    reply.send({
+      uuid,
+      timestamp
+    });
   });
 }
